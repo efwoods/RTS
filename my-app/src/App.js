d5a4957782f53ea3115cf1c12f64a096108c6d75
efwoods <22337004+efwoods@users.noreply.github.com>
2022-07-02 16:27:32 -0400

    router created nested routes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import { Link, Outlet } from "react-router-dom";
 
-function App() {
+function Home() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+       <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -17,9 +18,47 @@ function App() {
         >
           Learn React
         </a>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/contact">Contact</Link>
+        </nav>
       </header>
     </div>
   );
 }
 
-export default App;
+export function History () {
+  return(
+    <div>
+      <h1>Our History</h1>
+    </div>
+  )
+}
+
+export function About() {
+  return(
+    <div>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/contact">Contact</Link>
+        </nav>
+      <h1>About</h1>
+      <Outlet />
+    </div>
+  );
+}
+
+export function Contact() {
+  return(
+    <div>
+      <h1>Contact Us</h1>
+    </div>
+  );
+}
+
+
+export function App() {
+  return <Home />;
+}
