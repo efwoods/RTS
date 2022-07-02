c18fa59f8b8fd60430825394af6f2b7fd53f2102
efwoods <22337004+efwoods@users.noreply.github.com>
2022-07-02 17:05:59 -0400

    RTS added api call

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -18,8 +18,31 @@ export function History(){
   );
 } 
 
+function GithubUser({name, location}) {
+  return(
+    <div>
+      <h1>{name}</h1>
+      <p>{location}</p> 
+    </div>
+  );
+}
+
+
 export function App() {
-  return <Search />;
+    const [data, setData] = useState(null);
+    useEffect(() => {
+      fetch(
+        `https://api.github.com/users/moonhighway`
+      )
+        .then((response) => response.json())
+        .then(setData);
+    }, []);
+    if (data)
+      return <GithubUser 
+        name={data.name}
+        location={data.location} />;
+      
+      return <h1>Data</h1>
 }
 
 
