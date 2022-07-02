17fd6a5e45ec95f47c9a8a40bee2b821d9a3696b
efwoods <22337004+efwoods@users.noreply.github.com>
2022-07-02 16:45:33 -0400

    RTS checkpoint

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 
-function App() {
+export function Search(){
+  <div>
+    <h1>Search</h1>
+  </div>
+} 
+
+export function History(){
+  <div>
+    <h1>History</h1>
+  </div>
+} 
+
+
+
+export function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Search />
+  )
 }
 
-export default App;
+
