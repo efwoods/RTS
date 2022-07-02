bd020e615aff0bffd72b16a5c63b867894ddc14e
efwoods <22337004+efwoods@users.noreply.github.com>
2022-07-02 16:48:00 -0400

    RTS checkpoint

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -6,9 +6,13 @@ import {BrowserRouter,Routes,Route} from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <React.StrictMode>
-    <App library="React" />
-  //</React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route>
+        
+      </Route>
+    </Routes>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
